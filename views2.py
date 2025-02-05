a9318656cb1291528ca9c2db4e26e4b92641ba1c
Albaraa Mashal <77306932+AlbaraaM99@users.noreply.github.com>
2025-02-05 10:45:43 +0300

    Create views2.py

---
+++ views2.py
@@ -0,0 +1,23 @@
+// Part 3: Exercise: Display the Little Lemon available booking times
+@csrf_exempt
+def bookings(request):
+    if request.method == 'POST':
+        data = json.load(request)
+        exist = Booking.objects.filter(reservation_date=data['reservation_date']).filter(
+            reservation_slot=data['reservation_slot']).exists()
+        if exist==False:
+            booking = Booking(
+                first_name=data['first_name'],
+                reservation_date=data['reservation_date'],
+                reservation_slot=data['reservation_slot'],
+            )
+            booking.save()
+        else:
+            return HttpResponse("{'error':1}", content_type='application/json')
+    
+    date = request.GET.get('date',datetime.today().date())
+
+    bookings = Booking.objects.all().filter(reservation_date=date)
+    booking_json = serializers.serialize('json', bookings)
+
+    return HttpResponse(booking_json, content_type='application/json')
