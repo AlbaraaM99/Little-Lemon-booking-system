3dd3b015cfaafcf80ffbc713cf9e8dff74de3c7c
Albaraa Mashal <77306932+AlbaraaM99@users.noreply.github.com>
2025-02-05 10:39:00 +0300

    Create views.py

---
+++ views.py
@@ -0,0 +1,5 @@
+def bookings(request):
+    date = request.GET.get('date',datetime.today().date())
+    bookings = Booking.objects.all()
+    booking_json = serializers.serialize('json', bookings)
+    return render(request, 'bookings.html',{"bookings":booking_json})
